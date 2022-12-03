4c351cd7a00020a5e246870f8526f197d99cc8c0
hirokazu <n.hirokazu92@gmail.com>
2022-12-03 17:00:38 +0900

    ボタンコンポーネントの作成・装飾

---
+++ src/components/CButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- イベントハンドリング -->
+  <div class="button">ボタン</div>
+</template>
+
+<style>
+.button {
+  display: inline-block;
+  padding: 10px 30px;
+  background: #42b983;
+  color: #FFF;
+  border-top-right-radius: 10px;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  cursor: pointer;
+}
+</style>
---
+++ src/views/ButtonView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <CButton />
+  </div>
+</template>
+
+<script>
+import CButton from '@/components/CButton.vue'
+
+export default {
+  name: 'ButtonView',
+  components: {
+    CButton
+  }
+}
+</script>
