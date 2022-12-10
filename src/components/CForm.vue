6be1df345b1e10d276570cc2d0e2dae1bf97440c
hirokazu <n.hirokazu92@gmail.com>
2022-12-11 06:46:26 +0900

    入力フォームの実装

---
+++ src/components/CForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="form">
+    <div class="form-input">
+      <input type="text" v-model="textInput">
+      <p>{{ textInput }}</p>
+    </div>
+    <div class="form-input">
+      <textarea v-model="textAreaInput"></textarea>
+      <p>{{ textAreaInput }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function() {
+      return {
+        textInput: '',
+        textAreaInput: ''
+      };
+    }
+  }
+</script>
+
+<style>
+.form {
+  margin: 20px;
+}
+.form-input {
+  border-radius: 0;
+}
+</style>
---
+++ src/views/FormView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <CForm/>
+  </div>
+</template>
+
+<script>
+import CForm from '@/components/CForm.vue'
+
+  export default {
+    components: {
+      CForm
+    },
+  }
+</script>
